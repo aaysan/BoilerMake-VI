92fd7a08a14f784ad8b1b6d6d03cca934c4635e4
Ajay1997 <ajayGkumar97@gmail.com>
2018-10-21 02:55:28 -0400

    fetch js

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,14 @@
+//var http = require('http');
 function checkForNewData() {
+    console.log("data");
     const getData = async () => {
-      const data = await http.get({
-        url: "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/our_last_hackson-sfrvf/service/Capture_face/incoming_webhook/fetch_face"
-      });
-      console.log(data);
+    console.log("data");
+     const data = await fetch(
+        "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/our_last_hackson-sfrvf/service/Capture_face/incoming_webhook/fetch_face"
+    ).then(function(response) {
+        return response.json();
+    });
+     console.log(JSON.stringify(data));
       // Check if there is a new value
       // Handle any new values appropriately
       checkForNewData();
